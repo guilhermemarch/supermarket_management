5611aa5dabd4385a75d831a108f49a4289a78487
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2024-10-29 12:59:50 -0300

    estrutura do carrinho.java (falta implementar alguns metodos)

--- src/main/java/carrinho/modelo/Carrinho.java
+++ src/main/java/carrinho/modelo/Carrinho.java
@@ -5,7 +5,29 @@ import java.util.List;
 public class Carrinho {
 
     private List<Produto> produtos;
+    double valorTotalCarrinho;
 
+    public Carrinho(double valorTotalCarrinho, List<Produto> produtos) {
+        this.valorTotalCarrinho = valorTotalCarrinho;
+        this.produtos = produtos;
+    }
 
+    public void adicionarProduto(Produto produto, int quantidade) {
+
+        //verificar se o produto ja existe
+        //chamar metodo pra calcular o valor total do carrinho
+
+    }
+
+    public void removerProduto(int id) {
+        //percorrer a lista verificando o ID, caso seja igual remover usando metodo
+        //depois de remover chamar o metodo pra atualizar o valor total do carrinho
+
+    }
+
+    public void calcularValorTotal() {
+        //valor unitario * quantidade
+        //nao precisa retornar nada, apenas atualizar
+    }
 
 }
