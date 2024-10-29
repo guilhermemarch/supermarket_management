5d7c099ea8e41805bb65c73552c930700503da8e
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2024-10-29 09:49:51 -0300

    organizando pastas

--- src/main/java/carrinho/Program.java
+++ src/main/java/carrinho/Program.java
@@ -1,15 +1,16 @@
-package application;
+package carrinho;
 
 import java.sql.Connection;
-import java.sql.SQLException;
+
 import db.DB;
-import db.DbException;
 
 public class Program {
 
     public static void main(String[] args) {
         Connection conn = DB.getConnection();
             DB.closeConnection();
+
+
 
         }
     }
