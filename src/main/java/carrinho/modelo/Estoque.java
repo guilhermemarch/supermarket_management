b2bad39543d113864f2030e79ebd5263d5b430a0
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2024-10-29 12:51:09 -0300

    Alterações no estoque

--- src/main/java/carrinho/modelo/Estoque.java
+++ src/main/java/carrinho/modelo/Estoque.java
@@ -1,4 +1,62 @@
 package carrinho.modelo;
 
+import java.util.List;
+
 public class Estoque {
+
+    private List<Produto> produtos;
+
+    public Estoque(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+   public void adicionarProdutoEstoque(Produto produto, int quantidade) {
+            for (Produto p : produtos) {
+                if (p.getId() == produto.getId()) {
+                    p.setQuantidadeProduto(p.getQuantidadeProduto() + quantidade);
+                }
+            }
+            produto.setQuantidadeProduto(quantidade);
+            produtos.add(produto);
+
+   }
+
+    public void removerProdutoEstoque(long id, int quantidade) {
+        for (Produto p : produtos) {
+            if (p.getId() == id) {
+                try {
+                    if (p.getQuantidadeProduto() < quantidade) {
+                        throw new IllegalArgumentException("quantidade solicitada superior a quantidade disponível! quantidade disponível:" + p.getQuantidadeProduto());
+                    }
+                    p.setQuantidadeProduto(p.getQuantidadeProduto() - quantidade);
+                    if (p.getQuantidadeProduto() == 0) {
+                        produtos.remove(p);
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+                return;
+            }
+        }
+        System.out.println("Erro! Produto nao encontrado");
+    }
+
+
+
+   public Produto consultarProduto(long id) {
+        for (Produto p : produtos) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+       return null;
+   }
 }
--- src/main/java/carrinho/modelo/Produto.java
+++ src/main/java/carrinho/modelo/Produto.java
@@ -2,26 +2,26 @@ package carrinho.modelo;
 
 public class Produto {
 
-    private int codigoProduto;
+    private int id;
     private String nomeProduto;
     private String categoriaProduto;
     private double precoProduto;
     private int quantidadeProduto;
 
     public Produto(int codigoProduto, String nomeProduto, String categoriaProduto, double precoProduto, int quantidadeProduto) {
-        this.codigoProduto = codigoProduto;
+        this.id = codigoProduto;
         this.nomeProduto = nomeProduto;
         this.categoriaProduto = categoriaProduto;
         this.precoProduto = precoProduto;
         this.quantidadeProduto = quantidadeProduto;
     }
 
-    public int getCodigoProduto() {
-        return codigoProduto;
+    public int getId() {
+        return id;
     }
 
-    public void setCodigoProduto(int codigoProduto) {
-        this.codigoProduto = codigoProduto;
+    public void setId (int codigoProduto) {
+        this.id = codigoProduto;
     }
 
     public String getNomeProduto() {
